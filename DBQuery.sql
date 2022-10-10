ee6ac303433b56e636e911bfdc6a485cde966121
ThinhTranHuy <tranhuythinh97@gmail.com>
2022-10-10 17:54:51 +0700

    Add_DBQuery: Database creation

---
+++ DBQuery.sql
@@ -0,0 +1,8 @@
+create database Ecommerce on primary(
+ name = 'Ecom_Data',
+ filename = 'D:\LABS\Intern\NashTech\MidTerm\Ecom_Data.mdf'
+)
+ log on(
+ name = 'Ecom_Log',
+ filename = 'D:\LABS\Intern\NashTech\MidTerm\Ecom_Log.mdf'
+)
