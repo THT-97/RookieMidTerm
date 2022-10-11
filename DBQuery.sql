b2bd54bb07375c17b9dd0f51dc32b236051d8ef0
ThinhTranHuy <tranhuythinh97@gmail.com>
2022-10-11 09:36:41 +0700

    DBQuery: Table Creation  Category table added  Brand table added  Product table added

--- DBQuery.sql
+++ DBQuery.sql
@@ -6,3 +6,37 @@ create database Ecommerce on primary(
  name = 'Ecom_Log',
  filename = 'D:\LABS\Intern\NashTech\MidTerm\Ecom_Log.mdf'
 )
+
+USE Ecommerce
+
+CREATE TABLE Category(
+	CategoryID VARCHAR(5) PRIMARY KEY NOT NULL,
+	Name NVARCHAR(200) NOT NULL,
+	Description NVARCHAR(MAX) NULL,
+	Status TINYINT NOT NULL DEFAULT 1
+)
+
+CREATE TABLE Brand(
+	BrandID VARCHAR(5) PRIMARY KEY NOT NULL,
+	Name NVARCHAR(200) NOT NULL,
+	Description NVARCHAR(MAX) NULL,
+	Status TINYINT NOT NULL DEFAULT 1
+)
+
+CREATE TABLE Product(
+	ProductID VARCHAR(10) PRIMARY KEY NOT NULL,
+	Name NVARCHAR(200) NOT NULL,
+	Model VARCHAR(100) NULL,
+	Description NVARCHAR(MAX) NULL,
+	ListPrice MONEY NOT NULL,
+	SalePrice MONEY NULL,
+	Images VARCHAR(MAX) NULL DEFAULT 'default.png',
+	CreatedDate DATETIME NOT NULL DEFAULT GETDATE(),
+	UpdatedDate DATETIME NULL,
+	Quantity SMALLINT NOT NULL DEFAULT 1,
+	Rating TINYINT NULL DEFAULT 0,
+	RatingCount INT NULL DEFAULT 0,
+	Status TINYINT NOT NULL DEFAULT 1,
+	CategoryID VARCHAR(5) NOT NULL FOREIGN KEY(CategoryID) REFERENCES dbo.Category,
+	BrandID VARCHAR(5) NOT NULL FOREIGN KEY(BrandID) REFERENCES dbo.Brand
+)
