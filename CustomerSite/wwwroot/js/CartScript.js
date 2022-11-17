b017c816bfb180bff59b06a21cbb9e3b1fffd5ba
ThinhTranHuy <tranhuythinh97@gmail.com>
2022-11-17 23:24:48 +0700

    feat (CustomerSite): + add SaveCart function in javascript + create and save cart to sessionStorage when "Add to Cart" buttons are clicked

---
+++ CustomerSite/wwwroot/js/CartScript.js
@@ -0,0 +1,32 @@
+﻿function SaveCart(id, name, saleprice, listprice, colors, sizes) {
+    console.log(`${id} ${name} ${saleprice} ${listprice} ${colors} ${sizes}`);
+    let cart = JSON.parse(sessionStorage.getItem("cart"));
+    let count = Number.parseInt(sessionStorage.getItem("cart-count"));
+    isNaN(count) ? count = 1 : count += 1;
+    entity = {
+        id: id,
+        name: name,
+        saleprice: saleprice,
+        listprice: listprice,
+        colors: colors,
+        sizes: sizes,
+        quantity: 1
+    };
+    if (cart == null) cart = [entity];
+    else {
+        let dup = [...cart].findIndex(e => e.id == entity.id);
+        dup>=0 ? cart[dup].quantity += 1:
+        cart = [...cart, entity];
+    }
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+    sessionStorage.setItem("cart-count", count);
+    let cartElement = document.getElementById("cart-btn");
+    let cartCounterElement = document.getElementById("cart-counter");
+    if (cartCounterElement == null) {
+        cartCounterElement = document.createElement("div");
+        cartCounterElement.id = "cart-counter";
+        cartElement.appendChild(cartCounterElement);
+    }
+    cartCounterElement.innerHTML = `<small id="cart-count">${count}</small>`
+    console.log(cart);
+}
--- CustomerSite/wwwroot/js/site.js
+++ CustomerSite/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+window.onload = () => {
+    let count = Number.parseInt(sessionStorage.getItem("cart-count"));
+    if (!isNaN(count)) {
+        let cartElement = document.getElementById("cart-btn");
+        let cartCounterElement = document.getElementById("cart-counter");
+        if (cartCounterElement == null) {
+            cartCounterElement = document.createElement("div");
+            cartCounterElement.id = "cart-counter";
+            cartElement.appendChild(cartCounterElement);
+        }
+        cartCounterElement.innerHTML = `<small id="cart-count">${count}</small>`
+    }
+}
